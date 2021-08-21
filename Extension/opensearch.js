c7dc70a69197f11a3dcdd8b6c5b2fa5b5bc28f59
nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-08-21 20:04:42 +0800

    获取域名，获取 opensearch

--- Extension/opensearch.js
+++ Extension/opensearch.js
@@ -1,12 +1,27 @@
 const DEBUG = true;
 
+let url = new URL(window.location.href);
+let host = url.host;
+host = host.replace("www.", "");
+console.log(host);
+
+let xhr = new XMLHttpRequest();
+xhr.open(
+	"GET",
+	"https://raw.githubusercontent.com/lcandy2/oSearch/development/opensearch.json",
+	false
+);
+xhr.send();
+let data = JSON.parse(xhr.responseText)
+console.log(data.opensearch[host].title);
+
 // tab to search link
 let lnsearch = document.createElement("link");
 lnsearch.setAttribute("rel", "search");
 lnsearch.setAttribute("type", "application/opensearchdescription+xml");
 // should get from api
-lnsearch.setAttribute("title", "bilibili");
-lnsearch.setAttribute("href", "https://www.douban.com/opensearch");
+lnsearch.setAttribute("title", data.opensearch[host].title);
+lnsearch.setAttribute("href", data.opensearch[host].href);
 
 // suggestions link
 // ignore for now
@@ -17,20 +32,24 @@ lnsg.setAttribute("rel", "suggestions");
 // Special thanks to @gregsadetsky
 
 function setOpenSearch() {
-    if (document.querySelector('[type="application/opensearchdescription+xml"]')) { if (DEBUG) console.info('OpenSearch already existed'); } else {
-        document.getElementsByTagName("head")[0].appendChild(lnsearch);
-        if (DEBUG) console.info('OpenSearch was set');
-    }
+	if (
+		document.querySelector('[type="application/opensearchdescription+xml"]')
+	) {
+		if (DEBUG) console.info("OpenSearch already existed");
+	} else {
+		document.getElementsByTagName("head")[0].appendChild(lnsearch);
+		if (DEBUG) console.info("OpenSearch was set");
+	}
 } //setOpenSearch
 
 function onDOMContentLoaded() {
-    if (DEBUG) console.log('onDOMContentLoaded');
-    setOpenSearch();
-    window.addEventListener('load', function() {
-        if (DEBUG) console.log('onload');
-        setOpenSearch();
-        console.log('All done')
-    }); // #3
+	if (DEBUG) console.log("onDOMContentLoaded");
+	setOpenSearch();
+	window.addEventListener("load", function () {
+		if (DEBUG) console.log("onload");
+		setOpenSearch();
+		console.log("All done");
+	}); // #3
 } //onDOMContentLoaded
 
-document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
