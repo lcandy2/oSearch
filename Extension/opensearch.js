9c8ef47be5fcf3342ba2b71e8fd96ad9c44ba864
nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-08-22 18:56:47 +0800

    数据的获取与存储与读取

--- Extension/opensearch.js
+++ Extension/opensearch.js
@@ -5,15 +5,6 @@ let host = url.host;
 host = host.replace("www.", "");
 if (DEBUG) console.log("current: " + host);
 
-let xhr = new XMLHttpRequest();
-xhr.open(
-    "GET",
-    "https://raw.githubusercontent.com/lcandy2/oSearch/development/opensearch.json",
-    false
-);
-xhr.send();
-let data = JSON.parse(xhr.responseText)
-
 // suggestions link
 // ignore for now
 let lnsg = document.createElement("link");
@@ -22,7 +13,7 @@ lnsg.setAttribute("rel", "suggestions");
 // Codes below are based on https://github.com/gregsadetsky/chrome-dont-add-custom-search-engines/blob/master/src/content.js
 // Special thanks to @gregsadetsky
 
-function setOpenSearch() {
+function setOpenSearch(data) {
     if (
         document.querySelector('[type="application/opensearchdescription+xml"]')
     ) {
@@ -33,8 +24,8 @@ function setOpenSearch() {
         lnsearch.setAttribute("rel", "search");
         lnsearch.setAttribute("type", "application/opensearchdescription+xml");
         // should get from api
-        lnsearch.setAttribute("title", data.opensearch[host].title);
-        lnsearch.setAttribute("href", data.opensearch[host].href);
+        lnsearch.setAttribute("title", data.title);
+        lnsearch.setAttribute("href", data.href);
         if (DEBUG) console.log("title: " + lnsearch.getAttribute("title"));
         if (DEBUG) console.log("href: " + lnsearch.getAttribute("href"));
         document.getElementsByTagName("head")[0].appendChild(lnsearch);
@@ -42,16 +33,18 @@ function setOpenSearch() {
     }
 } //setOpenSearch
 
-function onDOMContentLoaded() {
+function onDOMContentLoaded(data) {
     if (DEBUG) console.log("onDOMContentLoaded");
-    setOpenSearch();
+    setOpenSearch(data);
     window.addEventListener("load", function() {
         if (DEBUG) console.log("onload");
-        setOpenSearch();
+        setOpenSearch(data);
         if (DEBUG) console.log("All done");
     }); // #3
 } //onDOMContentLoaded
 
-if(data.opensearch[host] !== undefined) {
-    document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
-}
+chrome.storage.local.get(['json'],(result) => {
+    if(result.json.opensearch[host] !== undefined) {
+        document.addEventListener("DOMContentLoaded", onDOMContentLoaded(result.json.opensearch[host]));
+    }
+});
